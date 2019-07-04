a22daf989df9cd1112bbe23299e5d50375786b5a
kang123lihua <634952030@qq.com>
2019-07-04 20:20:48 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -4,7 +4,15 @@ $(function(){
     //              (2) 密码不能为空, 且必须是 6-12 位
     // alert(123);
 $("#form").bootstrapValidator({
-        fields:{
+
+    // 配置图标
+    feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+    },
+
+    fields:{
             username:{
                 validators:{
                     notEmpty:{
@@ -14,6 +22,9 @@ $("#form").bootstrapValidator({
                         min:2,
                         max:6,
                         message:'用户名长度必须在 2-6 位'
+                    },
+                    callback:{
+                        message:'用户名不存在'
                     }
                 }
             },
@@ -26,12 +37,65 @@ $("#form").bootstrapValidator({
                         min:6,
                         max:12,
                         message:"密码长度必须在 6-12 位"
+                    },
+                    callback:{
+                        message:'密码错误'
+
                     }
                 }
             }
 
     }
 
+});
+
+
+$("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        // console.log($('#form').serialize());
+        $.ajax({
+            type: "post",
+            url: "/employee/employeeLogin",
+            dataType: "json",
+            data: $('#form').serialize(),
+            success: function( info ) {
+                // console.log( info );
+
+                if ( info.success ) {
+                    // alert( "登录成功" );
+                    location.href = "index.html";
+                }
+
+                if ( info.error === 1000 ) {
+                    // alert( "用户名不存在" )
+                    $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+                }
+
+                if ( info.error === 1001 ) {
+                    // alert( "密码错误" );
+                    // updateStatus
+                    // 参数1: 字段名称
+                    // 参数2: 校验状态
+                    // 参数3: 校验规则, 可以设置提示文本
+                    $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback")
+                }
+            }
+
+        })
+    });
+
+
+/*重置功能*/
+$('[type="reset"]').click(function(){
+    $('#form').data('bootstrapValidator').resetForm();
 })
 
+
+
+
+
 });
+
+
+
